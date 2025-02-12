f36bb554555d129b9f814c61441a936fd6da4202
ghivary <rais.alghivary4@gmail.com>
2025-02-12 16:14:42 +0700

    menambahkan paragraf lorem

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>COBA GIT REMOTE</title>
 </head>
 <body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum quam quibusdam natus architecto iure nisi veniam maxime magni provident ratione cumque fuga harum, eveniet inventore vero, facilis aliquam amet.</p>
     
 </body>
 </html>
